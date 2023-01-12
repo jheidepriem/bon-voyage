01bb0efa9de0f44427231eb8e072ef82c1a8e193
Josephine Heidepriem <jheidepriem@gmail.com>
2023-01-12 16:59:47 -0600

    Add trip tests

--- test/Trip-test.js
+++ test/Trip-test.js
@@ -3,38 +3,93 @@ const Trip = require("../src/Trip");
 
 describe("Trip", () => {
   let trip;
-  let tripData1;
-  let tripData2;
-  let tripData3;
-  
+
   beforeEach(() => {
-    tripData1 = {
-      id: 2,
-      userID: 7,
-      destinationID: 25,
-      travelers: 5,
-      date: "2022/06/01",
-      duration: 18,
-      status: "pending",
-    };
-    tripData2 = {
-      id: 55,
-      userID: 7,
-      destinationID: 62,
-      travelers: 11,
-      date: "2021/01/01",
-      duration: 25,
-      status: "approved",
-    };
-    tripData3 = {
-      id: 22,
-      userID: 7,
-      destinationID: 78,
-      travelers: 2,
-      date: "2020/06/01",
-      duration: 10,
-      status: "approved",
-    };
-   trip = new Trip(tripData);
-  })
+    const tripData = [
+      {
+        id: 22,
+        userID: 7,
+        destinationID: 22,
+        travelers: 2,
+        date: "2020/06/01",
+        duration: 10,
+        status: "approved",
+      },
+    ];
+    trip = new Trip(tripData);
+  });
+
+  it("should be a function", () => {
+    expect(Trip).to.be.a("function");
+  });
+
+  it("should be an instance of trip", () => {
+    expect(trip).to.be.an.instanceOf(Trip);
+  });
+
+  it("should find a trip by id", () => {
+    expect(trip.findTripByTripId(22)).to.deep.equal([
+      {
+        id: 22,
+        userID: 7,
+        destinationID: 22,
+        travelers: 2,
+        date: "2020/06/01",
+        duration: 10,
+        status: "approved",
+      },
+    ]);
+  });
+
+  it("should find a trip by destination id", () => {
+    expect(trip.findTripByDestinationId(22)).to.deep.equal([
+      {
+        id: 22,
+        userID: 7,
+        destinationID: 22,
+        travelers: 2,
+        date: "2020/06/01",
+        duration: 10,
+        status: "approved",
+      },
+    ]);
+  });
+
+  it("should find a trip by the number of travelers", () => {
+    expect(trip.retrieveNumTravelers(2)).to.deep.equal([
+      {
+        id: 22,
+        userID: 7,
+        destinationID: 22,
+        travelers: 2,
+        date: "2020/06/01",
+        duration: 10,
+        status: "approved",
+      },
+    ]);
+  });
+
+  it("should find a trip by duration", () => {
+    expect(trip.retrieveTripDuration(10)).to.deep.equal([
+      {
+        id: 22,
+        userID: 7,
+        destinationID: 22,
+        travelers: 2,
+        date: "2020/06/01",
+        duration: 10,
+        status: "approved",
+      },
+    ]);
+  });
+
+  it("should return undefined if there is no destination id", () => {
+    expect(trip.findTripByDestinationId(51)).to.equal(undefined);
+  });
+
+  it("should return undefined if there is no trip id", () => {
+    expect(trip.findTripByTripId(100)).to.equal(undefined);
+  });
+
+
 });
