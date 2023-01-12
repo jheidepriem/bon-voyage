54a912cd775652a53c398bbea477346b5bf8b70f
Josephine Heidepriem <jheidepriem@gmail.com>
2023-01-11 22:02:09 -0600

    Add yearly trip cost

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,11 +1,11 @@
 const dayjs = require('dayjs')
+
 class Traveler {
   constructor(travelerData) {
     this.userID = travelerData.userID;
     this.name = travelerData.name;
     this.trips = travelerData.trips
     }
-
 
 returnUserGreeting() {
     return this.name.split(" ")[0];
@@ -28,9 +28,28 @@ return futureTrips
 
 }
 
-calculateYearlyTripCost() {
-  
+calculateYearlyTripCost(destinations) {
+  let tripTotal = destinations.reduce((sum, destination) => {
+   this.trips.forEach(trip => {
+      if(trip.destinationID === destinations.id) {
+        sum += (destination.estimatedLodgingCostPerDay * trip.duration) + 
+        (destination.estimatedFlightCostPerPerson * trip.travelers)
+      }
+      return sum
+    }, 0)
+    return tripTotal
+    })
+  }
 }
 
-}
+  
+ 
+  
+  
+
+
+//if the destination id is the same as the trip id, multiply the destination. estimatedLodgingCostPerDay
+// by the trips.duration and add that to estimatedFlightCostPerPerson 8 this.trips.travelers
+
+
 module.exports = Traveler;
