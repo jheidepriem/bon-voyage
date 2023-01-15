28a17ed1dc5bbbfaaa183d4a2639d8db8fd4b815
Josephine Heidepriem <jheidepriem@gmail.com>
2023-01-15 15:56:38 -0600

    Delete all unused functions

--- src/Destinations.js
+++ src/Destinations.js
@@ -7,34 +7,11 @@ class Destinations {
     return this.data.filter((destination) => destination.id === id);
   }
 
-  findDestination() {
-    const result = this.data
-      .map((location) => {
-        return location.destination;
-      })
-      .sort((a, b) => {
-        return a.localeCompare(b);
-      });
-    return result;
-  }
-
   findIdByName(destinName) {
     const destinID = this.data.find((destination) => {
       return destination.destination === destinName;
     });
     return destinID.id;
-  }
-
-  findLodgingCost(lodgeCost) {
-    return this.data.filter(
-      (destination) => destination.estimatedLodgingCostPerDay === lodgeCost
-    );
-  }
-
-  findFlightCost(flightCost) {
-    return this.data.filter(
-      (destination) => destination.estimatedFlightCostPerPerson === flightCost
-    );
   }
 }
 
