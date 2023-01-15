9c97862befb811bf2d5d0a2c21741ae70eeb8ed3
Josephine Heidepriem <jheidepriem@gmail.com>
2023-01-15 15:57:50 -0600

    Delete unused tests/refactor

--- test/Trip-test.js
+++ test/Trip-test.js
@@ -43,48 +43,6 @@ describe("Trip", () => {
     expect(trip).to.be.an.instanceOf(Trip);
   });
 
-  it("should find a trip by destination id", () => {
-    expect(trip.findTripByDestinationId(22)).to.deep.equal([
-      {
-        id: 22,
-        userID: 7,
-        destinationID: 22,
-        travelers: 2,
-        date: "2020/06/01",
-        duration: 10,
-        status: "approved",
-      },
-    ]);
-  });
-
-  it("should find a trip by the number of travelers", () => {
-    expect(trip.retrieveNumTravelers(2)).to.deep.equal([
-      {
-        id: 22,
-        userID: 7,
-        destinationID: 22,
-        travelers: 2,
-        date: "2020/06/01",
-        duration: 10,
-        status: "approved",
-      },
-    ]);
-  });
-
-  it("should find a trip by duration", () => {
-    expect(trip.retrieveTripDuration(10)).to.deep.equal([
-      {
-        id: 22,
-        userID: 7,
-        destinationID: 22,
-        travelers: 2,
-        date: "2020/06/01",
-        duration: 10,
-        status: "approved",
-      },
-    ]);
-  });
-
   it("should calculate trip cost", () => {
     expect(trip.calculateTripCost(destinations, trips)).to.equal(2420);
   });
